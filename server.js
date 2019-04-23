54bdebef3e8d4492b741a992da447de4c90ed46b
Matias <matias.kovero@gmail.com>
2019-04-23 10:07:24 +0300

    removed TOKEN

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var app = express();
 var bodyParser = require('body-parser')
 const TelegramBot = require('node-telegram-bot-api');
-const TOKEN = process.env.TELEGRAM_TOKEN || '711574376:AAG5XYNeBgZhZO3jB5anMmgI_3c9LlopcU4';
+const TOKEN = process.env.TELEGRAM_TOKEN;
 const url = 'https://mk-telegram-bot.eu-gb.mybluemix.net';
 var port = process.env.PORT || 3000;
 
@@ -34,44 +34,46 @@ app.listen(port, function() {
     console.log("To view your app, open this link in your browser: http://localhost:" + port);
 });
 
+const startBot = async () => {
+  // Just to ping!
+  bot.on('message', msg => {
+    bot.sendMessage(msg.chat.id, 'I am alive!');
+  });
 
-// Just to ping!
-bot.on('message', msg => {
-  bot.sendMessage(msg.chat.id, 'I am alive!');
-});
+  // COMMAND /piato
+  bot.onText(/\/piato/, async (msg, match) => {      
+    const chatId = msg.chat.id;
+    var num = 0;
+    let lause = msg.text.split(' ');
+    if(lause[1] !== undefined) {
+        if (lause[1].trim() === "h") num = 1; // HUOMENNA
+        else if (lause[1].trim() === "yh") num = 2; // YLIHUOMENNA
+    };
+    var obj = await semmaApi();
+    var restaurant_name = obj.RestaurantName;
+    var week = obj.MenusForDays;
 
-// COMMAND /piato
-bot.onText(/\/piato/, async (msg, match) => {      
-  const chatId = msg.chat.id;
-  var num = 0;
-  let lause = msg.text.split(' ');
-  if(lause[1] !== undefined) {
-      if (lause[1].trim() === "h") num = 1; // HUOMENNA
-      else if (lause[1].trim() === "yh") num = 2; // YLIHUOMENNA
-  };
-  var obj = await semmaApi();
-  var restaurant_name = obj.RestaurantName;
-  var week = obj.MenusForDays;
+    var day = week[0];
+    var open_time = day.LunchTime;
+    var food = day.SetMenus;
 
-  var day = week[0];
-  var open_time = day.LunchTime;
-  var food = day.SetMenus;
-
-  var dayTxt = "_Tänään_";
-  if (num == 1) dayTxt = "_Huomenna_";
-  else if (num == 2) dayTxt = "_Ylihuomenna_";
-  var responseTxt = '*' + restaurant_name + '* ' + dayTxt + '\r\n';
-  if (open_time !== null) {
-    responseTxt += 'Lounas: ' + open_time + '\r\n';
-    for (i = 0; i < food.length; i++) {
-      responseTxt += '*' + food[i].Name + '* ';
-      responseTxt += '_' + food[i].Price + '_\r\n';
-      for (y = 0; y < food[i].Components.length; y++) {
-        responseTxt += food[i].Components[y].replace('*', '\\*') + '\r\n';
+    var dayTxt = "_Tänään_";
+    if (num == 1) dayTxt = "_Huomenna_";
+    else if (num == 2) dayTxt = "_Ylihuomenna_";
+    var responseTxt = '*' + restaurant_name + '* ' + dayTxt + '\r\n';
+    if (open_time !== null) {
+      responseTxt += 'Lounas: ' + open_time + '\r\n';
+      for (i = 0; i < food.length; i++) {
+        responseTxt += '*' + food[i].Name + '* ';
+        responseTxt += '_' + food[i].Price + '_\r\n';
+        for (y = 0; y < food[i].Components.length; y++) {
+          responseTxt += food[i].Components[y].replace('*', '\\*') + '\r\n';
+        }
       }
+    } else {
+      responseTxt += "Kiinni :(";
     }
-  } else {
-    responseTxt += "Kiinni :(";
-  }
-  bot.sendMessage(chatId, responseTxt, {parse_mode: 'Markdown'});
-});
+    bot.sendMessage(chatId, responseTxt, {parse_mode: 'Markdown'});
+  });
+};
+startBot();
