1be13f71fea7cd932836485597d29faa3b71ca13
Matias <matias.kovero@gmail.com>
2019-04-23 16:27:38 +0300

    added support for other semma restaurants

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var app = express();
 var bodyParser = require('body-parser')
 const TelegramBot = require('node-telegram-bot-api');
-const TOKEN = process.env.TELEGRAM_TOKEN;
+const TOKEN = process.env.TELEGRAM_TOKEN || "711574376:AAG5XYNeBgZhZO3jB5anMmgI_3c9LlopcU4";
 const url = 'https://mk-telegram-bot.eu-gb.mybluemix.net';
 var port = process.env.PORT || 3000;
 
@@ -33,50 +33,32 @@ var port = process.env.PORT || 3000
 app.listen(port, function() {
     console.log("To view your app, open this link in your browser: http://localhost:" + port);
 });
+bot.on('message', msg => {
+  const chatId = msg.chat.id;
 
-// COMMAND /piato
+  const message = msg.text;
+
+  bot.sendMessage(chatId, message, {parse_mode: 'HTML'});
+});
+
+/**  START ---  SEMMA RESTAURANTS --- */
 bot.onText(/\/piato/, async (msg, match) => {      
   const chatId = msg.chat.id;
-  var num = 0;
-  let lause = msg.text.split(' ');
-  if(lause[1] !== undefined) {
-      if (lause[1].trim() === "h") num = 1; // HUOMENNA
-      else if (lause[1].trim() === "yh") num = 2; // YLIHUOMENNA
-  };
-  var obj = await getPiato;
-  var restaurant_name = obj.RestaurantName;
-  var week = obj.MenusForDays;
+  var obj = await getPiato();
+  var responseTxt = parseSemma(msg, obj);
 
-  var day = week[0];
-  var open_time = day.LunchTime;
-  var food = day.SetMenus;
-
-  var dayTxt = "_Tänään_";
-  if (num == 1) dayTxt = "_Huomenna_";
-  else if (num == 2) dayTxt = "_Ylihuomenna_";
-  var responseTxt = '*' + restaurant_name + '* ' + dayTxt + '\r\n';
-  if (open_time !== null) {
-    responseTxt += 'Lounas: ' + open_time + '\r\n';
-    for (i = 0; i < food.length; i++) {
-      responseTxt += '*' + food[i].Name + '* ';
-      responseTxt += '_' + food[i].Price + '_\r\n';
-      for (y = 0; y < food[i].Components.length; y++) {
-        responseTxt += food[i].Components[y].replace('*', '\\*') + '\r\n';
-      }
-    }
-  } else {
-    responseTxt += "Kiinni :(";
-  }
   bot.sendMessage(chatId, responseTxt, {parse_mode: 'Markdown'});
 });
 
 bot.onText(/\/lozzi/, async (msg, match) => {
   const chatId = msg.chat.id;
-  var obj = await getLozzi;
+  var obj = await getLozzi();
   var responseTxt = parseSemma(msg, obj);
 
   bot.sendMessage(chatId, responseTxt, {parse_mode: 'Markdown'});
 });
+
+/** END --- SEMMA RESTAURANTS --- */
 
 // Supporting function to easily parse Semma API objects
 function parseSemma(msg, obj) {
@@ -111,5 +93,3 @@ function parseSemma(msg, obj) {
   }
   return responseTxt;
 };
-
-startBot();
--- utils/semma.js
+++ utils/semma.js
@@ -1,9 +1,23 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
 
+//TODO: redo the code, way too much copy+paste
+
+// ALL API URLS
+const piato_url = 'https://www.semma.fi/modules/json/json/Index?costNumber=1408&language=fi';
+const lozzi_url = 'https://www.semma.fi/modules/json/json/Index?costNumber=1401&language=fi';
+const maija_url = 'https://www.semma.fi/modules/json/json/Index?costNumber=1402&language=fi';
+const libri_url = 'https://www.semma.fi/modules/json/json/Index?costNumber=141301&language=fi';
+const tilia_url = 'https://www.semma.fi/modules/json/json/Index?costNumber=1413&language=fi';
+const syke_url = 'https://www.semma.fi/modules/json/json/Index?costNumber=1405&language=fi';
+const rentukka_url = 'https://www.semma.fi/modules/json/json/Index?costNumber=1416&language=fi';
+const ylisto_url = 'https://www.semma.fi/modules/json/json/Index?costNumber=1403&language=fi';
+const fiilu_url = 'https://fiilu-scraper.now.sh/lunch/week';
+const ilokivi_url = 'https://ilokivi-scraper.now.sh/lunch/today?semmaFormat=true';
+
 const getPiato = async () => {
   let menu;
-  let url = 'https://www.semma.fi/modules/json/json/Index?costNumber=1408&language=fi';
+  let url = piato_url;
   try {
     const response = await axios.get(url);
     if (response.status === 200) {
@@ -19,7 +33,135 @@ const getPiato = async () => {
 }
 const getLozzi = async () => {
   let menu;
-  let url = 'https://www.semma.fi/modules/json/json/Index?costNumber=1401&language=fi';
+  let url = lozzi_url;
+  try {
+    const response = await axios.get(url);
+    if (response.status === 200) {
+      const html = response.data;
+      menu = html;
+    } else {
+      throw new Error(`Error, response status ${response.status}`);
+    }
+  } catch (error) {
+    return [];
+  }
+  return menu;
+}
+const getMaija = async () => {
+  let menu;
+  let url = maija_url;
+  try {
+    const response = await axios.get(url);
+    if (response.status === 200) {
+      const html = response.data;
+      menu = html;
+    } else {
+      throw new Error(`Error, response status ${response.status}`);
+    }
+  } catch (error) {
+    return [];
+  }
+  return menu;
+}
+const getLibri = async () => {
+  let menu;
+  let url = libri_url;
+  try {
+    const response = await axios.get(url);
+    if (response.status === 200) {
+      const html = response.data;
+      menu = html;
+    } else {
+      throw new Error(`Error, response status ${response.status}`);
+    }
+  } catch (error) {
+    return [];
+  }
+  return menu;
+}
+const getTilia = async () => {
+  let menu;
+  let url = tilia_url;
+  try {
+    const response = await axios.get(url);
+    if (response.status === 200) {
+      const html = response.data;
+      menu = html;
+    } else {
+      throw new Error(`Error, response status ${response.status}`);
+    }
+  } catch (error) {
+    return [];
+  }
+  return menu;
+}
+const getSyke = async () => {
+  let menu;
+  let url = syke_url;
+  try {
+    const response = await axios.get(url);
+    if (response.status === 200) {
+      const html = response.data;
+      menu = html;
+    } else {
+      throw new Error(`Error, response status ${response.status}`);
+    }
+  } catch (error) {
+    return [];
+  }
+  return menu;
+}
+const getRentukka = async () => {
+  let menu;
+  let url = rentukka_url;
+  try {
+    const response = await axios.get(url);
+    if (response.status === 200) {
+      const html = response.data;
+      menu = html;
+    } else {
+      throw new Error(`Error, response status ${response.status}`);
+    }
+  } catch (error) {
+    return [];
+  }
+  return menu;
+}
+const getYlisto = async () => {
+  let menu;
+  let url = ylisto_url;
+  try {
+    const response = await axios.get(url);
+    if (response.status === 200) {
+      const html = response.data;
+      menu = html;
+    } else {
+      throw new Error(`Error, response status ${response.status}`);
+    }
+  } catch (error) {
+    return [];
+  }
+  return menu;
+}
+const getFiilu = async () => {
+  let menu;
+  let url = fiilu_url;
+  try {
+    const response = await axios.get(url);
+    if (response.status === 200) {
+      const html = response.data;
+      menu = html;
+    } else {
+      throw new Error(`Error, response status ${response.status}`);
+    }
+  } catch (error) {
+    return [];
+  }
+  return menu;
+}
+const getIlokivi = async () => {
+  let menu;
+  let url = ilokivi_url;
   try {
     const response = await axios.get(url);
     if (response.status === 200) {
@@ -36,5 +178,13 @@ const getLozzi = async () => {
 
 module.exports = {
   getPiato,
-  getLozzi
+  getLozzi,
+  getMaija,
+  getLibri,
+  getTilia,
+  getSyke,
+  getRentukka,
+  getYlisto,
+  getFiilu,
+  getIlokivi
 }
