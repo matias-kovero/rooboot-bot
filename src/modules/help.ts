0e5fccd6919f25b9d0041c8282c9b48ee21631f9
Matias <matias.kovero@gmail.com>
2020-09-07 17:56:31 +0300

    Fixed lint issues

--- src/modules/help.ts
+++ src/modules/help.ts
@@ -1,5 +1,6 @@
 import { TelegrafContext } from "telegraf/typings/context";
+import { Message } from "telegraf/typings/telegram-types";
 
-export default async(ctx: TelegrafContext) => {
+export default async(ctx: TelegrafContext): Promise<Message> => {
   return ctx.replyWithMarkdown(`*Need help?* \r\nCan't help you, sorry.`);
 }
