5280f3f416e7a8b83ae64d2712543de3297f4b49
Matias <matias.kovero@gmail.com>
2020-09-23 11:24:48 +0300

    Reworded sentences.

--- src/modules/fingerpori.ts
+++ src/modules/fingerpori.ts
@@ -5,20 +5,21 @@ import { addChat, removeChat } from '../utils/database/queries/fingerpori';
 export default async(ctx: TelegrafContext): Promise<Message> => {
   const chat_id = ctx.chat?.id.toString();
   const text = ctx.message.text;
-  let message = '*Brrts*⚙️ \r\nBroke my code.';
+  let message;
 
   switch(text) {
     case '/tilaafp':
       const status = await addChat(chat_id);
-      const t = new Date(); 
-      message = status != null ? `You are already subcribed for daily comics!` : `You are now subcribed for daily comics :)`;
-      message += `\r\nThey should arrive @ _06:00 UTC_\r\nTime now: _${t.getUTCHours()}:${t.getUTCMinutes()} UTC_`;
+      const state = status != null ? 'already' : 'now';
+      const t = new Date();
+      message = `You are ${state} subcribed for daily comics!\r\nThey should arrive ~ _06:00 UTC_\r\nTime now: _${t.getUTCHours()}:${t.getUTCMinutes()} UTC_`;
       break;
     case '/perufptilaus':
       const response = await removeChat(chat_id);
       message = response.deletedCount > 0 ? `Removed this chat from daily comics!` : `Silly, you should subscribe first ;)`;
       break;
     default: // Something is seriosly broken if we land here.
+      message = '*Brrts*⚙️ \r\nBroke my code.';
       break;
   }
 
